8b8d2093046f688f53b8b54343c0736c85dabc55
Trent Corey <trentcorey15@gmail.com>
2022-04-09 17:56:55 -0400

    Added buyer-seller transactions function

--- opensea_collection.py
+++ opensea_collection.py
@@ -8,6 +8,7 @@ import scipy.stats
 import powerlaw
 import math
 from math import log10
+import pickle
 
 collectionCSVs = [
     "0n1_force.csv",
@@ -1092,6 +1093,63 @@ class collection():
         for (obs, exp) in zip(observed, expected):
             chiSquare = chiSquare + (obs - exp)**2/exp
         return chiSquare
+
+    def buyer_seller_txns(self):
+        # Load in pickle file containing dict of seller transactions
+        cwd = os.getcwd()
+        collection = self.name[:-4]
+        pickleLocation = cwd + "/" + collection + ".pkl"
+        if (not os.path.exists(pickleLocation)):
+            print("ERROR: " + pickleLocation + " DOES NOT EXIST")
+            return
+        inFile = open(pickleLocation, "rb")
+        sellerTxns = pickle.load(inFile)
+        inFile.close()
+
+        # Read in buyers & sellers associated with each transaction
+        readIn = ['winner_account_address', 'seller_address']
+        buyersLocation = cwd + "/data/" + collection + ".csv"
+        if (not os.path.exists(buyersLocation)):
+            print("ERROR: " + buyersLocation + " DOES NOT EXIST")
+            return
+        nftSales = pd.read_csv(buyersLocation, usecols = readIn)
+        nftSales.dropna(subset=['seller_address'], inplace=True)
+
+        # List to store buyers that received ETH from sellers
+        # Stored as buyer, seller
+        buyersSellers = []
+
+        for index, row in nftSales.iterrows():
+            for addr in sellerTxns[row['seller_address']]:
+
+                # If the address from txn list is the one who bought the NFT and received ETH...
+                if(addr == row['winner_account_address'] and
+                sellerTxns[row['seller_address']][addr] == "sent"):
+                        buyersSellers.append(tuple((addr, row['seller_address'])))
+        
+        # Write list of suspicious buyers/sellers to file
+        # Might not be necessary?
+        outFileLoc = cwd + "/chain_analysis_results/" + collection + "_results.txt" 
+        outFile = open(outFileLoc, "w+")
+        for pair in buyersSellers:
+            line = ' '.join(str(tmp) for tmp in pair)
+            outFile.write(line + '\n')
+        if len(buyersSellers) > 0:
+            print("/chain_analysis_results/" + collection + "_results.txt successfully written")
+        else:
+            print(collection + " sellers did not send ETH to buyers")
+        outFile.close()
+
+        # Make list of sellers with dupes removed, make array with number of nonoffenders & offenders
+        data = np.array([len(nftSales.index) - len(buyersSellers), len(buyersSellers)])
+        myColors = ['skyblue', 'red']
+        pieLabels = ["Did not send ETH to buyer (" + str(data[0]) + ")", "Sent ETH to buyer (" + str(data[1]) + ")"]
+        myExplode = [0.2,0]
+
+        # Display pie chart of result
+        plt.pie(data, labels=pieLabels, explode=myExplode, colors=myColors)
+        plt.title("Seller Transaction history for " + collection +  " (" + str(len(nftSales.index)) + " total transactions)", bbox={'facecolor':'0.8', 'pad':5})
+        plt.show()
 
 if __name__ == '__main__':
     """
